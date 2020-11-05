dd5441c7511bfb57d9318a3a21f8a9d011332480
amara mootaz <45128408+mootaz11@users.noreply.github.com>
2020-11-05 11:40:37 +0100

    adding user picture to chat

--- screens/Conversation.js
+++ screens/Conversation.js
@@ -116,7 +116,7 @@ export default function Conversation(props) {
                             return (
                                    message.sender._id == context.user._id ?
                                     (<View style={styles.messageSentContainer} key={index}>
-                                        <Image style={styles.userImage} source={require("../assets/mootaz.jpg")} />
+                                        <Image style={styles.userImage} source={{uri:context.user.photo}} />
                                         <View style={dark ? styles.messageSentDark :styles.messageSent}>
                                               <Text style={dark ? styles.textMessageDark : styles.textMessage}>{message.content}</Text>
                                            { message.content.includes("mon code est   :") ? 
